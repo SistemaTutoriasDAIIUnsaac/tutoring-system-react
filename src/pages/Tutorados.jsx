6805cb692fde05a09b340cb01082512f477dc825
Charlie <161368@unsaac.edu.pe>
2021-08-25 10:56:52 -0500

    update pages ListStudent an Tutorados on file Pages

--- src/pages/Tutorados.jsx
+++ src/pages/Tutorados.jsx
@@ -1,203 +1,151 @@
-import React, { Fragment, useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+import MaterialTable from "material-table";
+import { MuiThemeProvider,createMuiTheme,FormControlLabel,Switch} from "@material-ui/core";
+import { Link } from "react-router-dom";
 
-const Prueba = () => {
-  const[datos,setDatos]=useState({
-    cod_student:'',
-    name: '',
-    f_lastname: '',
-    m_lastname: '',
-    phone:'',
-    email:'',
-    cod_faculty:'',
-    cod_career:'',
-    adress:''
-  })  
-  const Data1 ={
-    nro_orden: '1',
-    cod_student:'161234',
-    name: 'Miguel',
-    f_lastname: 'Shaw',
-    m_lastname: 'Trauco',
-    estado:'Completado',
+function Tutorados() {
+  //Button of mode and module dark (table)
+  const[preferDark,setPreferDark]=useState(()=>{
+    const mode=localStorage.getItem("_tableDarkMode")
+    return mode==="true"||false
+  }) 
+  const theme=createMuiTheme({
+    palette:{
+      type:preferDark?"dark":"light"
+    }
+  })
+  const handelDarkModeChange=()=>{
+    setPreferDark(!preferDark)
+    localStorage.setItem("_tableDarkMode",!preferDark)
   }
-  const Data2 ={
-    nro_orden: '2',
-    cod_student:'165678',
-    name: 'Paolo',
-    f_lastname: 'Kane',
-    m_lastname: 'Guerrero',
-    nro_citas:'2',
-    estado:'Completado',
-  }  
-  const Data3 ={
-    nro_orden: '3',
-    cod_student:'171234',
-    name: 'Cristian',
-    f_lastname: 'Palenca',
-    m_lastname: 'Cueva',
-    estado:'Indispuesto',
-  }
-  const Data4 ={
-    nro_orden: '4',
-    cod_student:'175678',
-    name: 'André',
-    f_lastname: 'Sterling',
-    m_lastname: 'Carrillo',
-    estado:'Completado',
-  }    
-  const onSubmit = (e) => {
-    //e.preventDefault();
-    //postData();
-  };
-  const url = "https://tutoring-system-api.herokuapp.com/students";
-  const postData = async () => {
-    const res = await axios.post(url, datos);
-    console.log(res.data);
-  };
-  const CambiarEntrada = (event) => {
-    //console.log(event.target.value)
-    setDatos({
-        ...datos,            
-        [event.target.name]:event.target.value
-    })
-    console.log(datos)
-}
+  //Finish Button and module of mode dark (table)
+  const [DataProducts, setDataProducts] = useState([]);
+  const columns = [
+    { title: "Nro", field: "no_service" },
 
-const EnviarDatos = (event) =>{
-    event.preventDefault();
-    console.log(datos.nombre+' '+datos.apellido)
-}  
+    { title: "Código", field: "code" },
+
+    { title: "Nombres y Apellidos", field: "full_name" },
+
+    { title: "Estado", field: "nro_citas" },
+  ];
+
+  const queryAPI = async (method = "get", data = {}) => {
+    const url = "https://tsc-rest-api.herokuapp.com/products";
+    var ans = [];
+    if (method == "get") {
+      ans = await axios.get(url);
+      setDataProducts(ans.data);
+    } else if (method == "delete") {
+      // console.log(data);
+      ans = await axios.delete(
+        "https://tsc-rest-api.herokuapp.com/product/" + data.id
+      );
+      // console.log(ans);
+      setDataProducts(DataProducts.filter((item) => item.id != data.id));
+    }
+  };
+
+  const filterOptions = (inputValue, _data) => {
+    return _data.filter((i) =>
+      i.label.toUpperCase().includes(inputValue.toUpperCase())
+    );
+  };
+
+  useEffect(() => {
+    queryAPI("get");
+  }, [setDataProducts]);
+
   return (
-    <Fragment>
-      <div className="content-wrapper">
-        {/* Content Header (Page header) */}
-        <section className="content-header">
-          <div className="container-fluid">
-            <div className="row mb-2">
-              <div className="col-sm-6">
-                <h1>Lista de Tutorados</h1>
-              </div>
-              <div className="col-sm-6">
-                <ol className="breadcrumb float-sm-right">                  
-                  <li className="breadcrumb-item">
-                    {/* <a href="#">Home</a> */}
-                    {/* <Link to="Detalle_Servicio"> Detalle Servicio</Link> */}
-                  </li>
-                </ol>
-              </div>
+    <div className="content-wrapper">
+      <section className="content-header">
+        <div className="container-fluid">
+          <div className="row mb-2">
+            <div className="col-sm-6">
+              <h1>Lista de Tutorados</h1>
+            </div>
+            <div className="col-sm-6">
+              <ol className="breadcrumb float-sm-right">
+                <li className="breadcrumb-item active">Tutorados</li>
+                <li className="breadcrumb-item">
+                  {/* <a href="#">Home</a> */}
+                  <Link to="Nuevo_Estudiante"> Nuevo Tutorado</Link>
+                </li>
+              </ol>
             </div>
           </div>
-          {/* /.container-fluid */}
-        </section>
-        {/* Main content */}
-        <section className="content">
-          <div className="container-fluid">
-            <div className="card card-primary">
-              {/* /.card-header */}
-              {/* form start */}
-              <div className="card">
-                <div className="card-header">
-                  <h3 className="card-title">Tutorados</h3>
-                </div>
+        </div>
+        {/* /.container-fluid */}
+      </section>
+      {/* Main content */}
+      <section className="content">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-12">
+              <div className="card">                
                 {/* /.card-header */}
+                {/*Start button of mode dark*/}
+                <FormControlLabel
+                  value="top"
+                  control={<Switch color="primary" checked={preferDark}/>}
+                  onChange={handelDarkModeChange}
+                  label="Modo Oscuro"
+                  labelPlacement="left"
+                />
+                {/*Finish button of mode dark*/}                                        
+                {/*Start mode dark or light*/}
+                <MuiThemeProvider theme={theme}>
                 <div className="card-body">
-                  <table className="table table-bordered">
-                    <thead>
-                      <tr>
-                        <th style={{ width: 10 }}>#</th>
-                        <th style={{ width: 100 }}>Código</th> 
-                        <th>Nombres y Apellidos</th>
-                        <th style={{ width: 80 }}>Estado</th>
-                        <th style={{ width: 40 }}>Información</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td>{Data1.nro_orden+ "."}</td>
-                        <td>{Data1.cod_student}</td>
-                        <td>{Data1.name+" "+Data1.f_lastname+" "+Data1.m_lastname}</td>
-                        <td>{Data1.estado}</td>
-                        <td>
-                        <button size="small" type="submit" className="btn btn-info" onClick={(e) => onSubmit(e)}>
-                          _Ver_                
-                        </button>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>{Data2.nro_orden+ "."}</td>
-                        <td>{Data2.cod_student}</td>
-                        <td>{Data2.name+" "+Data2.f_lastname+" "+Data2.m_lastname}</td>
-                        <td>{Data2.estado}</td>
-                        <td>
-                        <button size="small" type="submit" className="btn btn-info" onClick={(e) => onSubmit(e)}>
-                            _Ver_
-                        </button>                          
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>{Data3.nro_orden+ "."}</td>
-                        <td>{Data3.cod_student}</td>
-                        <td>{Data3.name+" "+Data3.f_lastname+" "+Data3.m_lastname}</td>
-                        <td>{Data3.estado}</td>
-                        <td>
-                        <button size="small" type="submit" className="btn btn-info" onClick={(e) => onSubmit(e)}>
-                            _Ver_
-                        </button>                          
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>{Data4.nro_orden+ "."}</td>
-                        <td>{Data4.cod_student}</td>
-                        <td>{Data4.name+" "+Data4.f_lastname+" "+Data4.m_lastname}</td>
-                        <td>{Data4.estado}</td>
-                        <td>
-                        <button size="small" type="submit" className="btn btn-info" onClick={(e) => onSubmit(e)}>
-                            _Ver_
-                        </button>                          
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
+                    <MaterialTable
+                      title="Tutorados"
+                      data={DataProducts}
+                      columns={columns}
+                      options={{
+                        search: true,
+                        paging: true,
+                        filtering: true,
+                        exportButton: true,
+                        actions: true,
+                        actionsColumnIndex: -1,
+                        pageSize: 10,
+                      }}
+                      actions={[
+                        {
+                          icon: "delete",
+                          tooltip: "Borrar Estudiante",
+                          onClick: (ev, rowData) =>
+                            console.log("Borrar Estudiante"),
+                        },
+                        {
+                          icon: "edit",
+                          tooltip: "Editar Estudiante",
+                          onClick: (ev, rowData) =>
+                            console.log("Editar Estudiante"),
+                        },
+                      ]}
+                    />                  
+                  <Link to="/">
+                    <button type="submit" className="btn btn-danger ml-8 mt-3">
+                      Volver
+                    </button>
+                  </Link>
                 </div>
                 {/* /.card-body */}
-                <div className="card-footer clearfix">
-                  <ul className="pagination pagination-sm m-0 float-right">
-                    <li className="page-item">
-                      <a className="page-link" href="#">
-                        «
-                      </a>
-                    </li>
-                    <li className="page-item">
-                      <a className="page-link" href="#">
-                        1
-                      </a>
-                    </li>
-                    <li className="page-item">
-                      <a className="page-link" href="#">
-                        2
-                      </a>
-                    </li>
-                    <li className="page-item">
-                      <a className="page-link" href="#">
-                        3
-                      </a>
-                    </li>
-                    <li className="page-item">
-                      <a className="page-link" href="#">
-                        »
-                      </a>
-                    </li>
-                  </ul>
-                </div>
+                {/*Finish mode dark or light*/}
+                </MuiThemeProvider>
               </div>
+              {/* /.card */}
             </div>
+            {/* /.col */}
           </div>
-          {/* /.container-fluid */}
-        </section>
-        {/* /.content */}
-      </div>
-    </Fragment>
+          {/* /.row */}      
+        </div>
+        {/* /.container-fluid */}
+      </section>
+      {/* /.content */}
+    </div>
   );
-};
-export default Prueba;
+}
+
+export default Tutorados;
