b61eab74548d8de7ada4a9301ce051b73ea93b84
Charlie <161368@unsaac.edu.pe>
2021-08-25 22:01:08 -0500

    Update button Generar Cita on ListStudent and add page Talleres on file Pages

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Teacher from './pages/Teacher';
 import NewStudent from './pages/NewStudent';
 import ListStudent from './pages/ListStudent';
 import Tutorados from './pages/Tutorados';
+import Talleres from './pages/Talleres';
 
 import ListStudents from './pages/ListStudents';
 import InformationStudent from './pages/InformationStudent';
@@ -24,6 +25,7 @@ function App() {
         <Route exact path="/prueba" component={Prueba}/>
         <Route exact path="/teacher" component={Teacher}/>
         <Route exact path="/Tutorados" component={Tutorados}/>
+        <Route exact path="/Talleres" component={Talleres}/>
         <Route exact path="/Nuevo_Estudiante" component={NewStudent}/>
         <Route exact path="/list_student" component={ListStudent}/>
         <Route exact path="/Lista_de_Estudiantes" component={ListStudents}/>
