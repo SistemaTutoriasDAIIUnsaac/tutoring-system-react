d85383b5ac82daf590ac3ce64401edaf6a4d9b1a
WidmarO <171259@unsaac.edu.pe>
2021-08-29 22:33:44 -0500

    fixed problems with appointments state file

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
     <Router>
       <Header />
       <SideBar />
-      <AppointmentsState>
+      {/* <AppointmentsState> */}
         <Switch>
           {/* <Route exact path="/prueba" component={Prueba}/> */}
           <Route exact path="/teacher" component={Teacher} />
@@ -71,7 +71,7 @@ function App() {
             component={VerListaPorAlumno}
           />
         </Switch>
-      </AppointmentsState>
+      {/* </AppointmentsState> */}
       <Footer />
     </Router>
   );
