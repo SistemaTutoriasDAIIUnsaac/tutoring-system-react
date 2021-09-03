52653fb0250482174ca0cb0e32b9442d1732e3c2
cristian1938 <cristian_1938@hotmail.com>
2021-09-03 01:23:38 -0500

    Add NewNovelty in pages folder

--- src/App.js
+++ src/App.js
@@ -22,6 +22,8 @@ import AppointmentsState from "./context/Appointments/AppointmentsState";
 import AuthState from "./context/Authentication/authState";
 import EditTeacherList from "./pages/EditTeacherList"
 import EditHelperStudentList from "./pages/EditHelperStudentList"
+import NewNovelty from "./pages/NewNovelty";
+
 // import Login from "./auth/Login";
 // import Register from "./auth/Register";
 
@@ -50,6 +52,7 @@ function App() {
               <Route exact path="/Lista_Asistencia/:cod_taller" component={VerListaPorAlumno} />
               <Route exact path="/Editar_Lista_De_Tutores" component={EditTeacherList} />
               <Route exact path="/Editar_Lista_De_Estudiantes_Ayudantes" component={EditHelperStudentList} />
+              <Route exact path="/Informacion_Coordinador/:cod_Cor" component={NewNovelty} />
             </Switch>
           <Footer />
         </Router>
