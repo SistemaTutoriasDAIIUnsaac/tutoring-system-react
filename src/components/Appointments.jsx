7fd1661b60dd7839bbb9470f001de49c36074668
Nadiabeth15 <nadiita1015@gmail.com>
2021-09-03 22:58:03 -0500

    Modified Appointments.jsx to get the appointment data by clicking anywhere on the card in the appointment list.

--- src/components/Appointments.jsx
+++ src/components/Appointments.jsx
@@ -4,18 +4,19 @@ import { Link } from "react-router-dom";
 function Appointments() {
   return (
     <Fragment>
+      <Link to="Datos_de_citas">
          <div className="card card-secondary">
         <div className="card-header" style={{ padding: 5 }}>
-        <Link to="Datos_de_citas">
+        
           <h5 className="card-title">Cita</h5>
-          </Link>
+          
         </div> 
         <div className="card-body pt-2 pb-0">
           <form>
             <div className="row">
               <div className="col-sm-1">
                 <div className="form-group">
-                  <label>N° Cita</label>
+                  <label style={{color:"#292929"}}>N° Cita</label>
                   <div className="input-group">
                     <input type="text" className="form-control" disabled />
                   </div>
@@ -23,13 +24,13 @@ function Appointments() {
               </div>
               <div className="col-sm-3">
                 <div className="form-group">
-                  <label>Fecha</label>
+                  <label style={{color:"#292929"}}>Fecha</label>
                   <input type="text" className="form-control" disabled/>
                 </div>
               </div>
               <div className="col-sm-8">
                 <div className="form-group">
-                  <label>Descripción</label>
+                  <label style={{color:"#292929"}}>Descripción</label>
                   <div className="input-group">
                     <div className="input-group-prepend"></div>
                     <input type="text" class="form-control" disabled="true"></input>
@@ -41,6 +42,7 @@ function Appointments() {
         </div>
         
       </div>
+      </Link>
     </Fragment>
   )
 }
