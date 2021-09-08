6c7e397040d747b1e0e2d5628f0b6b65df38f284
WidmarO <171259@unsaac.edu.pe>
2021-09-07 21:01:36 -0500

    specify manually the base url for clinteAxios

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const clienteAxios = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL  
+  baseURL: "https://tutoring-system-api.herokuapp.com"
 })
 
 export default clienteAxios;
